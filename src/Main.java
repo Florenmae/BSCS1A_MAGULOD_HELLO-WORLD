0e6755d1e959dbcb8ad16f268635dbcd172a5d0b
Florenmae <magulodren@gmail.com>
2022-04-16 08:30:29 +0800

    lab01

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Create a java program that will display your
+ * "Hello World!"
+ * name
+ * and your section
+ */
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Erolle Jenfree G. Magulod");
+        System.out.println("BSCS 1A");
+    }
+}
